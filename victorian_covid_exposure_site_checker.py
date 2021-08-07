43e5eaeffc49ecfc098edccfbfb360bc974a5cd8
mipy1 <76726158+mipy1@users.noreply.github.com>
2021-08-07 13:51:08 +1000

    🦧 Message for no exposures

--- victorian_covid_exposure_site_checker.py
+++ victorian_covid_exposure_site_checker.py
@@ -9,10 +9,18 @@ data = requests.get(url).json()
 with open("locations.json", "r") as locations_file:
     locations = json.load(locations_file)['locations']
 
+
+match = False
+
 # Compares "locations" and "data" and prints matches
 for exposure in data["result"]["records"]:
     if exposure['Suburb'] in locations:
+        match = True
         print("\n\033[1m" + f"{exposure['Site_title']}" + "\033[0m")
         print(f"Suburb\t\t-   {exposure['Suburb']}")
         print(f"Exposure Date\t-   {exposure['Exposure_date']}")
         print(f"Exposure Time\t-   {exposure['Exposure_time']}")
+
+# If no matches are found
+if match == False:
+    print("No Exposures On Record")
